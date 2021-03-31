7e776c9f9eece46d1029b5fb07e802f27a4baa25
Rob Salas <59486764+salasrob@users.noreply.github.com>
2021-03-31 08:56:38 -0500

    cleaned up unused files, styling update, resume bug fix

--- myserver/myapp/src/app/app.module.ts
+++ myserver/myapp/src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule } from '@angular/common/http'
-
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { TitleComponent } from './pages/title/title.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-
+import { AppComponent } from './app.component';
+import { TitleComponent } from './pages/title/title.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +27,6 @@ import { ProjectsComponent } from './pages/projects/projects.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
