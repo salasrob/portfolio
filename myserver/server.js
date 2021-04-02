28bb42a6fdfc4d80100eb5bc48514adac272ad98
Rob Salas <59486764+salasrob@users.noreply.github.com>
2021-04-02 16:21:39 -0500

    heroku build fix

--- myserver/server.js
+++ myserver/server.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const path = require('path');
-// const PORT = process.env.PORT || 5000;
-// var app_path = 'myapp/dist/myapp';
+const PORT = process.env.PORT || 5000;
+var app_path = 'myapp/dist/myapp';
 
-// app.use('/', express.static(path.join(__dirname, app_path)))
+app.use('/', express.static(path.join(__dirname, app_path)))
 
-app.listen(process.env.PORT || 4000, () => { console.log('Server Started') })
+app.listen(PORT, () => { console.log('Server Started') })
 
 app.use(express.json());
 app.use(cors())
