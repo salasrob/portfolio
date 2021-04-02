071b75152f785e8752a8221ca7500d1f39269ee5
Rob Salas <59486764+salasrob@users.noreply.github.com>
2021-04-02 16:40:44 -0500

    heroku build test

--- myserver/server.js
+++ myserver/server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var app_path = 'myapp/dist/myapp';
+var app_path = './myapp/dist/myapp';
 
 app.use('/', express.static(path.join(__dirname, app_path)))
 
